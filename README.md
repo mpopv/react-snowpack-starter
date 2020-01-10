bc01d24978cedeafedef81aa71dba344b7bbefd4
Matt Popovich <mattpopovich@outlook.com>
2020-01-10 09:04:09 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-snowpack-starter
+
+An opinionated starter project with React, Babel, Yarn, and Snowpack to bundle ES modules.
---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "/web_modules/react.js";
+
+let App = () => {
+  return <div>Hello world</div>;
+};
+export default App;
